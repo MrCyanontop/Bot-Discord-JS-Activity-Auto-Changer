953ab6add48135283275a591178865313ed6e63b
MrCyanontop <157929566+MrCyanontop@users.noreply.github.com>
2024-01-30 19:52:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ client.on('ready', () => { // When the bot turns on
             `${client.guilds.cache.size} servers`, // Enables the bot to show how many servers it's in, in the status
             `${client.channels.cache.size} channels`, // Enables the bot to show how many channels it's in, in the status
             "Music...", // Enables the bot to send a message of your choice
-        ]
+        ];
 
-        const status = statuses[Math.floor(Math.random() * statuses.length)] // Chooses a random list from statuses and puts it into a variable.
-        client.user.setActivity(status, { type: "LISTENING" }) // Status changer - WATCHING / LISTENING / STREAMING / DND / ONLINE
+        const status = statuses[Math.floor(Math.random() * statuses.length)]; // Chooses a random list from statuses and puts it into a variable.
+        client.user.setActivity(status, { type: "LISTENING" }); // Status changer - WATCHING / LISTENING / STREAMING / DND / ONLINE
 
-    }, 
-
-            30000) // Time for status to change - Recommended  = 20,000 (20 Seconds) - API doesn't really allow less values but it will work
-
+    }, 1000); // Time for status to change - Recommended = 20,000 (20 Seconds) - API doesn't really allow less values but it will work
 });
